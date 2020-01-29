ef1aa88382c6df9f53fb4d1dbf218f810a757b38
AlexisJTSE <alexis6630@gmail.com>
2020-01-29 09:11:27 +0100

    Translate Evenement

--- src/main/webapp/app/entities/evenement/evenement-detail.tsx
+++ src/main/webapp/app/entities/evenement/evenement-detail.tsx
@@ -2,13 +2,11 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Link, RouteComponentProps } from 'react-router-dom';
 import { Button, Row, Col } from 'reactstrap';
-import { Translate, ICrudGetAction } from 'react-jhipster';
+import { Translate } from 'react-jhipster';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { IRootState } from 'app/shared/reducers';
 import { getEntity } from './evenement.reducer';
-import { IEvenement } from 'app/shared/model/evenement.model';
-import { APP_DATE_FORMAT, APP_LOCAL_DATE_FORMAT } from 'app/config/constants';
 
 export interface IEvenementDetailProps extends StateProps, DispatchProps, RouteComponentProps<{ id: string }> {}
 
@@ -22,12 +20,12 @@ export const EvenementDetail = (props: IEvenementDetailProps) => {
     <Row>
       <Col md="8">
         <h2>
-          <Translate contentKey="pycataApp.evenement.detail.title">Evenement</Translate> [<b>{evenementEntity.id}</b>]
+          Evenement [<b>{evenementEntity.id}</b>]
         </h2>
         <dl className="jh-entity-details">
           <dt>
             <span id="intitule">
-              <Translate contentKey="pycataApp.evenement.intitule">Intitule</Translate>
+              Intitule
             </span>
           </dt>
           <dd>{evenementEntity.intitule}</dd>
@@ -35,14 +33,14 @@ export const EvenementDetail = (props: IEvenementDetailProps) => {
         <Button tag={Link} to="/evenement" replace color="info">
           <FontAwesomeIcon icon="arrow-left" />{' '}
           <span className="d-none d-md-inline">
-            <Translate contentKey="entity.action.back">Back</Translate>
+            Retour
           </span>
         </Button>
         &nbsp;
         <Button tag={Link} to={`/evenement/${evenementEntity.id}/edit`} replace color="primary">
           <FontAwesomeIcon icon="pencil-alt" />{' '}
           <span className="d-none d-md-inline">
-            <Translate contentKey="entity.action.edit">Edit</Translate>
+            Editer
           </span>
         </Button>
       </Col>
